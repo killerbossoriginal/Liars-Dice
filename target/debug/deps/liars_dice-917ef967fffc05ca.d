195a55c00dba41f772474d77ad9d2aef9484f29e
KillerBossOriginal <killerbossoriginal@outlook.it>
2024-02-09 10:34:25 +0100

    First Commit

---
+++ target/debug/deps/liars_dice-917ef967fffc05ca.d
@@ -0,0 +1,5 @@
+/home/killerboss/online-dev/liars-dice/target/debug/deps/libliars_dice-917ef967fffc05ca.rmeta: src/main.rs
+
+/home/killerboss/online-dev/liars-dice/target/debug/deps/liars_dice-917ef967fffc05ca.d: src/main.rs
+
+src/main.rs:
